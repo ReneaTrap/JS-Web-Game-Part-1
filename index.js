916c9df3d5bd19b6b699d77feb2fbea01724f457
ReneaTrap <reneatrapani@gmail.com>
2023-08-30 22:14:35 -0500

    more changes done, have images back

--- index.js
+++ index.js
@@ -4,36 +4,36 @@ function newImage( url, left, bottom){
     let imgElement = document.createElement('img')
     imgElement.src = url
     imgElement.style.position = 'fixed'
-    imgElement.style.left;
-    imgElement.style.bottom;
+    imgElement.style.left + "px"
+    imgElement.style.bottom + "px"
     document.body.append(imgElement);
     return imgElement
       
 }
 
-let item1 = newImage('assets/green-character.gif', '100px', '100px');
-let item2 = newImage('assets/pine-tree.png', '450px', '200px');
-let item3 = newImage('assets/tree.png', ' 200px', '300px');
-let item4 = newImage('assets/pillar.png', '350px', '100px');
-let item5 = newImage('assets/crate.png', '150px', '425px');
-let item6 = newImage('assets/well.png', '150px', '425px');
+let character = newImage('assets/green-character.gif', '100px', '100px');
+let pinetree = newImage('assets/pine-tree.png', '450px', '200px');
+let tree = newImage('assets/tree.png', '200px', '300px');
+let pillar = newImage('assets/pillar.png', '350px', '100px');
+let crate = newImage('assets/crate.png', '150px', '425px');
+let well = newImage('assets/well.png', '150px', '425px');
 
-let item7 = newImage('assets/sword.png', '500px', '405px');
-let item8 = newImage('assets/shield.png', '165px','185px');
-let item9 = newImage('assets/staff.png','600px', '100px');
+let sword = newImage('assets/sword.png', '500px', '405px');
+let shield = newImage('assets/shield.png', '165px','185px');
+let staff = newImage('assets/staff.png', '600px', '100px');
 
 function newItem(item) {
     item.addEventListener('dblclick', function () {
         console.log(item);
     });
-    newItem(item1);
-newItem(item2);
-newItem(item3);
-newItem(item4);
-newItem(item5);
-newItem(item6);
-newItem(item7);
-newItem(item8);
-newItem(item9);
 }
-         
+
+newItem(character);
+newItem(pinetree);
+newItem(tree);
+newItem(pillar);
+newItem(crate);
+newItem(well);
+newItem(sword);
+newItem(shield);
+newItem(staff);
